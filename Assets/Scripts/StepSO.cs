fab1e4aeb78cfe692a907b97aa3678d4e569205a
Denis <alch007@gmail.com>
2020-10-08 19:50:07 +0300

    Lesson 7

---
+++ Assets/Scripts/StepSO.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Step", fileName = "New Step")]
+public class StepSO : ScriptableObject
+{
+    [TextArea(15, 50)]
+    public string content;
+    public string location;
+    public Sprite sprite;
+
+    public StepSO[] nextSteps;
+}
---
+++ Assets/Scripts/TextQuestSO.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextQuestSO : MonoBehaviour
+{
+    public Text contentText;
+    public Text locationText;
+    public Image image;
+
+    public StepSO startStep;
+
+    public StepSO currentStep;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentStep = startStep;
+        UpdateElements();
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            CheckNumber(0);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            CheckNumber(1);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            CheckNumber(2);
+        }
+    }
+
+    private void CheckNumber(int number)
+    {
+        if (currentStep.nextSteps.Length > number)
+        {
+            if (currentStep.nextSteps[number] != null)
+            {
+                currentStep = currentStep.nextSteps[number];
+                UpdateElements();
+            }
+        }
+    }
+
+    private void UpdateElements()
+    {
+        contentText.text = currentStep.content;
+        locationText.text = currentStep.location;
+        image.sprite = currentStep.sprite;
+    }
+}
